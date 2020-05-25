d7cc050f4e57ddaefb0b303176d9a1a888989188
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2020-05-24 20:14:45 -0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-def my_hash_func(key: str) -> int:
-  # this returns "ascii" value of a character
-  ascii_val = ord(key[0].lower()) 
-  return ascii_val - 97
-
-print(my_hash_func('zEbRas have white stripes'))
